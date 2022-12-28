7fea4eb02144adc263c0e68ebecb56d1d50f06d6
Antonis Koufis <antokoufis@gmail.com>
2022-12-28 19:34:39 +0200

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.png';
+import './App.css';
+ 
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mx-auto mt-5">
+                <img src={logo} className="App-logo" alt="logo"/>
+              <h1 className= "mt-5">NFTAuctionhouse</h1>
+              <p>
+                Edit <code>src/frontend/components/App.js</code> and save to reload.
+              </p>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

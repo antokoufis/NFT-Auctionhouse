4b010195b745bbc9641c887b4ee8049e6ab71fc6
Antonis Koufis <antokoufis@gmail.com>
2022-12-29 20:18:35 +0200

    Create Auctionhouse Smart Contract

---
+++ src/backend/contracts/Auctionhouse.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Auctionhouse is ReentrancyGuard {
+    // the account that receives fees
+    address payable public immutable feeAccount;
+    // the fee percentage on sales
+    uint256 public immutable feePercent;
+    uint256 public itemCount;
+
+    constructor(uint256 _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
+}
